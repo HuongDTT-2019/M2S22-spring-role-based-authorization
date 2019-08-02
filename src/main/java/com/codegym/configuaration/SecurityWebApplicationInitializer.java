8526a2e11d616c53d8455112396a412e8ebf58fd
Huong-DTT <thuhuong160994@gmail.com>
2019-08-02 09:18:30 +0700

    spring-role-based-authorization

---
+++ src/main/java/com/codegym/configuaration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.codegym.configuaration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@EnableWebSecurity
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
